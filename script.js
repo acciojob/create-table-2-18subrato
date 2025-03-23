410b26f4dede8c5879d99a6e6b84a516f1327927
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-23 16:19:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 function createTable() {
-    //Write your code here
+
+	let rn = prompt("Input number of rows")
+	let cn = prompt("Input number of columns")
+
+	rn = +rn;
+	cn = +cn;
+
+	if(isNaN(rn) || isNaN(cn) || rn<=0 || cn<=0){
+		alert("Invalid input please enter positive val for rn and cn")
+		return;
+	}
+
+	let table = document.getElementById("myTable")
+	table.innerHTML= "";
+
+	for (let i = 0; i < rn; i++) {
+        const row = document.createElement("tr");
+        for (let j = 0; j < cn; j++) {
+            const cell = document.createElement("td"); 
+            cell.textContent = `Row-${i} Column-${j}`; 
+            row.appendChild(cell); 
+        }
+        table.appendChild(row); 
+    }
   
 }
